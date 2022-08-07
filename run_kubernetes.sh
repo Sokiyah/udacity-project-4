549b3dccdca81f47377ed3784092a764139ace06
Sokiyah <sokee.a@gmail.com>
2022-08-07 23:28:05 +0100

    updated kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath=sokee/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacity-project4 --image=$dockerpath --port=8080
+kubectl run udacity-project4 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
